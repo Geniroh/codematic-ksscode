62aa469969b500f4522cc6dee09e8a80c04a1ee1
Geniroh2 <chibuzor@codematic.io>
2024-12-19 00:21:15 +0100

    project setup

---
+++ auth.ts
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/prisma";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [],
+  session: {
+    strategy: "jwt",
+  },
+});
